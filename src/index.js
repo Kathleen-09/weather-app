44166d7db5dcdad402726c0e489ff40edf1f8cb1
Kathleen-09 <kathleen09jag@gmail.com>
2025-04-02 12:23:46 -0400

    Injected more weather data

--- src/index.js
+++ src/index.js
@@ -2,10 +2,32 @@ function refreshWeather (response){
 let temperatureElement=document.querySelector("#temperature");
 let temperature= response.data.temperature.current;
 let cityElement=document.querySelector("#city");
+let descriptionElement=document.querySelector("#description");
+let humidityElement=document.querySelector("#humidity");
+let windSpeedElement=document.querySelector("#wind-speed");
+let timeElement=document.querySelector("#time");
+let date= new Date (response.data.time*1000);
 
 cityElement.innerHTML=response.data.city;
+
+descriptionElement.innerHTML=response.data.condition.description;
+humidityElement.innerHTML=`${response.data.temperature.humidity}% `;
+windSpeedElement.innerHTML= `${response.data.wind.speed}km/hr`
 temperatureElement.innerHTML=Math.round(temperature);
+timeElement.innerHTML= formatDate(date);
   
+}
+
+function formatDate(date){
+  
+  let minutes=date.getMinutes();
+  let hours=date.getHours();
+  let days= ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  let day=days[date.getDay()];
+  if (minutes<10){
+    minutes=`0${minutes}`;
+  }
+  return`${day} ${hours}:${minutes}`;
 }
 
 
@@ -29,5 +51,5 @@ searchCity(searchInput.value);
 
 let searchFormElement=document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
-searchCity(Durban);
+searchCity("Durban");
 
